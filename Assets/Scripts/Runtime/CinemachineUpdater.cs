4a83e397275d18775cd5de7564ade20580bffadc
boxfriend <cawills394@gmail.com>
2024-02-11 15:52:57 -0500

    component to update cinemachine on Update instead of LateUpdate

---
+++ Assets/Scripts/Runtime/CinemachineUpdater.cs
@@ -0,0 +1,15 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CinemachineUpdater : MonoBehaviour
+{
+    [SerializeField] private CinemachineBrain _brain;
+    private void Awake () => _brain.m_UpdateMethod = CinemachineBrain.UpdateMethod.ManualUpdate;
+    public void Update () => _brain.ManualUpdate();
+
+    private void Reset ()
+    {
+        _brain = GetComponent<CinemachineBrain>();
+        _brain.m_UpdateMethod = CinemachineBrain.UpdateMethod.ManualUpdate;
+    }
+}
