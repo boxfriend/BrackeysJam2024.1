08cb4a26b48597f67081e177aa8585cdac8bdbc1
boxfriend <cawills394@gmail.com>
2024-02-12 17:47:30 -0500

    door component for handling SFX and VFX when shooting doors

---
+++ Assets/Scripts/Runtime/Door.cs
@@ -0,0 +1,26 @@
+﻿using Boxfriend.Utils;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _deathParticles;
+    [SerializeField] private Transform _particleSpawnPoint;
+
+    [SerializeField] private AudioResource _hitSound;
+    [SerializeField] private AudioResource _breakSound;
+
+    public void OnDie()
+    {
+        //AudioManager.Instance.Play(_breakSound, transform.position);
+        var particles = Instantiate(_deathParticles, _particleSpawnPoint.position, _particleSpawnPoint.rotation);
+        Destroy(particles.gameObject, 3f);
+        Destroy(gameObject);
+    }
+
+    public void OnDamage()
+    {
+        AudioManager.Instance.Play(_hitSound, transform.position);
+        //Do something visual here
+    }
+}
