c6633aaedd9ada7559ab2eb5cc81ad3b79b4eb00
boxfriend <cawills394@gmail.com>
2024-02-12 17:47:13 -0500

    component for taking damage

---
+++ Assets/Scripts/Runtime/DamageTaker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DamageTaker : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private UnityEvent<int> _onDamage;
+    [SerializeField] private UnityEvent _onDeath;
+    public int Health { get; private set; }
+
+    private void Awake () => Health = _maxHealth;
+
+    public void Damage(int damage)
+    {
+        Health -= damage;
+        
+        _onDamage?.Invoke(damage);
+
+        if (Health <= 0)
+        {
+            _onDeath?.Invoke();
+        }
+    }
+}
