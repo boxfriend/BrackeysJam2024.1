4cf278f861de2526daa595f2374f1c214ce668e7
boxfriend <cawills394@gmail.com>
2024-02-12 17:48:15 -0500

    component for handling destruction of bullets and dealing damage

---
+++ Assets/Scripts/Runtime/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private int _damage;
+
+    public void AddDamageModifier (int amount) => _damage = Mathf.Max(_damage + amount, 0);
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        Debug.Log(collision.gameObject, collision.gameObject);
+        if (collision.collider.TryGetComponent(out DamageTaker damager))
+            damager.Damage(_damage);
+
+        Destroy(gameObject);
+    }
+}
