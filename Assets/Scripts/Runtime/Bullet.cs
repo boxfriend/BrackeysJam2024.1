3781dbfc56c1e7cb60c4c07203a7483613fac96b
boxfriend <cawills394@gmail.com>
2024-02-13 14:52:43 -0500

    Bullet component now handles its rigidbody instead of PlayerShooter

--- Assets/Scripts/Runtime/Bullet.cs
+++ Assets/Scripts/Runtime/Bullet.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private int _damage;
+    [SerializeField] private Rigidbody _rigidbody;
+    public event Action<Bullet> OnCollide;
 
     public void AddDamageModifier (int amount) => _damage = Mathf.Max(_damage + amount, 0);
+
+    public void Freeze()
+    {
+        _rigidbody.angularVelocity = Vector3.zero;
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.isKinematic = true;
+    }
+
+    public void UnFreeze () => _rigidbody.isKinematic = false;
+    public void Shoot (Vector3 shootForce) => _rigidbody.AddRelativeForce(shootForce, ForceMode.Impulse);
 
     private void OnCollisionEnter (Collision collision)
     {
@@ -12,6 +25,6 @@ public class Bullet : MonoBehaviour
         if (collision.collider.TryGetComponent(out DamageTaker damager))
             damager.Damage(_damage);
 
-        Destroy(gameObject);
+        OnCollide?.Invoke(this);
     }
 }
