83a8b338b50e4c5be33f2c2726959ebb93d4408f
boxfriend <cawills394@gmail.com>
2024-02-13 16:57:48 -0500

    will now properly load the desired scene and unload the loadscreen

--- Assets/Scripts/Runtime/SceneChanger.cs
+++ Assets/Scripts/Runtime/SceneChanger.cs
@@ -14,9 +14,15 @@ public static class SceneChanger
 
         var loadScene = SceneManager.LoadSceneAsync((int)scene, LoadSceneMode.Additive);
         loadScene.allowSceneActivation = false;
-        await loadScene;
-        await SceneManager.UnloadSceneAsync((int)Scene.Loadscreen);
+        while (loadScene.progress < 0.9f)
+            await Awaitable.NextFrameAsync();
+
         loadScene.allowSceneActivation = true;
+        while (!loadScene.isDone)
+            await Awaitable.NextFrameAsync();
+
+        await Awaitable.NextFrameAsync();
+        await SceneManager.UnloadSceneAsync((int)Scene.Loadscreen);
     }
 
 }
