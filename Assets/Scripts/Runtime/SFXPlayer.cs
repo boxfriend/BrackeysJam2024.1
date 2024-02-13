98bf1ca625df41cd95356ce0f06c460124947cf5
boxfriend <cawills394@gmail.com>
2024-02-12 21:48:37 -0500

    component to play a sound through AudioManager

---
+++ Assets/Scripts/Runtime/SFXPlayer.cs
@@ -0,0 +1,13 @@
+﻿using Boxfriend.Utils;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SFXPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioResource _soundResource;
+    [SerializeField] private Transform _soundPosition;
+
+    public void PlaySound () => AudioManager.Instance.Play(_soundResource, _soundPosition.position);
+
+    private void Reset () => _soundPosition = transform;
+}
