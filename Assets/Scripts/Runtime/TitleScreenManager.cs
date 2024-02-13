ec8cb390dc54d5d635c0706eed68e7f8bd518a7f
boxfriend <cawills394@gmail.com>
2024-02-13 16:58:11 -0500

    manager for title screen stuff, WIP

---
+++ Assets/Scripts/Runtime/TitleScreenManager.cs
@@ -0,0 +1,19 @@
+﻿using Michsky.MUIP;
+using UnityEngine;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    [SerializeField] private ButtonManager _resetScoreButton;
+
+    private void Awake ()
+    {
+        if (PlayerPrefs.GetInt(ScoreTracker.HighScoreKey) == 0)
+            _resetScoreButton.Interactable(false);
+    }
+
+    public void ResetScore () => ScoreTracker.ResetHighScore();
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+    public void PlayGame () => SceneChanger.ChangeScene(Scene.GameScene);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+}
