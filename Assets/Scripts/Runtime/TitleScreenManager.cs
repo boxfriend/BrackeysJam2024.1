2460dafa4a695ca8245b3b93d3678f70c3e95710
boxfriend <cawills394@gmail.com>
2024-02-13 21:47:10 -0500

    sends OnShoot message to player for shooting in title screen

--- Assets/Scripts/Runtime/TitleScreenManager.cs
+++ Assets/Scripts/Runtime/TitleScreenManager.cs
@@ -6,6 +6,7 @@ public class TitleScreenManager : MonoBehaviour
     [SerializeField] private ButtonManager _resetScoreButton;
 
     [SerializeField] private string _discordInviteURL;
+    [SerializeField] private PlayerShooter _player;
     private void Awake ()
     {
         if (PlayerPrefs.GetInt(ScoreTracker.HighScoreKey) == 0)
@@ -19,4 +20,6 @@ public class TitleScreenManager : MonoBehaviour
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
     public void DiscordInvite () => Application.OpenURL(_discordInviteURL);
+
+    public void PlayerShoot () => _player.SendMessage("OnShoot");
 }
