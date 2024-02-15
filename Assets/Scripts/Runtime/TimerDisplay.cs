509c1cba89f23b7d82572acb6d0ea8fa8231e94e
boxfriend <cawills394@gmail.com>
2024-02-14 22:31:28 -0500

    timer for the end of the game

---
+++ Assets/Scripts/Runtime/TimerDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Michsky.MUIP;
+using UnityEngine;
+
+public class TimerDisplay : MonoBehaviour
+{
+    [SerializeField] private ProgressBar _display;
+    [SerializeField] private float _countDownTime;
+    private float _timer;
+
+    public event Action OnTimerEnd;
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+        _display.minValue = 0;
+        _display.maxValue = _countDownTime;
+        _display.valueLimit = _countDownTime;
+        _display.ChangeValue(_countDownTime);
+        _timer = _countDownTime;
+    }
+
+    private void Update ()
+    {
+        _timer -= Time.deltaTime;
+        _display.ChangeValue(_timer);
+        
+
+        if(_timer <= 0)
+        {
+            OnTimerEnd?.Invoke();
+        }
+    }
+}
