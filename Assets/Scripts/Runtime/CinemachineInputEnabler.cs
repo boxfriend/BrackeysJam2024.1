20c62f02ff63fa1d61244fa7c56657aafb6a5d4e
boxfriend <cawills394@gmail.com>
2024-02-14 22:27:04 -0500

    enable/disable cinemachine input on pause

---
+++ Assets/Scripts/Runtime/CinemachineInputEnabler.cs
@@ -0,0 +1,15 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CinemachineInputEnabler : MonoBehaviour
+{
+    [SerializeField] private CinemachineInputProvider _input;
+
+    public void OnPause (bool isPause)
+    {
+        if (isPause)
+            _input.XYAxis.action.Disable();
+        else
+            _input.XYAxis.action.Enable();
+    }
+}
