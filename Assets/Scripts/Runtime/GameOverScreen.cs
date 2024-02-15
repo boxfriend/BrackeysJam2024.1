f8038388cbee619390ba91acd7ef888647d779a3
boxfriend <cawills394@gmail.com>
2024-02-14 22:31:11 -0500

    handles the game over screen for switching scenes and activating itself

---
+++ Assets/Scripts/Runtime/GameOverScreen.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void Quit () => SceneChanger.ChangeScene(Scene.Title);
+    public void Restart () => SceneChanger.ChangeScene(Scene.GameScene);
+
+    public void SetActive (bool active) => gameObject.SetActive(active);
+}
