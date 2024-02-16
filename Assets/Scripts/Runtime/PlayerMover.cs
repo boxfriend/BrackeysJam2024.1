b58282336dfd60f39d1b9129672e9f4cee59984c
boxfriend <cawills394@gmail.com>
2024-02-15 21:30:31 -0500

    updates move speed when move speed item is collected

--- Assets/Scripts/Runtime/PlayerMover.cs
+++ Assets/Scripts/Runtime/PlayerMover.cs
@@ -5,9 +5,22 @@ public class PlayerMover : MonoBehaviour
 {
     [SerializeField] private SplineController _controller;
     [SerializeField] private SplineAnimate _splineAnimate;
+    [SerializeField] private float _minimumMoveSpeed;
 
-    private void Start () => _splineAnimate.Container = _controller.Container;
-    
+    private void Start ()
+    {
+        Item.OnMoveSpeedChange += OnMoveSpeedChange;
+        _splineAnimate.Container = _controller.Container;
+    }
+
+    private void OnDestroy () => Item.OnMoveSpeedChange -= OnMoveSpeedChange;
+    private void OnMoveSpeedChange (float speed)
+    {
+        var oldSpeed = _splineAnimate.MaxSpeed;
+        oldSpeed = Mathf.Max(oldSpeed + speed, _minimumMoveSpeed);
+        _splineAnimate.MaxSpeed = oldSpeed;
+    }
+
     public void OnPause(bool isPaused)
     {
         if (isPaused)
